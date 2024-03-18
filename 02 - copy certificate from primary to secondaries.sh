33148684f158f7f5e69f7b962a21f99fe3d5b3f7
Mohammad Falahat <23@Buluttakin.com>
2024-03-18 12:12:38 +0330

    added powershell cert copy commands

--- 02 - copy certificate from primary to secondaries.sh
+++ 02 - copy certificate from primary to secondaries.sh
@@ -22,3 +22,34 @@ kubectl cp $NAMESPACE ag_certificate.key  $podags1:/var/opt/mssql
 echo Copying AG certificates from localhost to pod $podags2
 kubectl cp $NAMESPACE ag_certificate.cert $podags2:/var/opt/mssql
 kubectl cp $NAMESPACE ag_certificate.key  $podags2:/var/opt/mssql
+
+
+
+
+
+
+
+### IN POWERSHELL:
+
+# Define the namespace
+$NAMESPACE='appraisal'
+
+# Retrieve the pod names
+$podagpm = kubectl get pods -n ${NAMESPACE} -l app=appraisal-instance1 -o custom-columns=:metadata.name | Select-Object -Last 1
+$podags1 = kubectl get pods -n ${NAMESPACE} -l app=appraisal-instance2 -o custom-columns=:metadata.name | Select-Object -Last 1
+$podags2 = kubectl get pods -n ${NAMESPACE} -l app=appraisal-instance3 -o custom-columns=:metadata.name | Select-Object -Last 1
+
+# Copy certificates from AG primary to localhost
+Write-Host "Copying AG certificates from ${podagpm} to localhost"
+kubectl cp ${NAMESPACE}/${podagpm}:/var/opt/mssql/ag_certificate.cert ag_certificate.cert
+kubectl cp ${NAMESPACE}/${podagpm}:/var/opt/mssql/ag_certificate.key  ag_certificate.key
+
+# Copy certificates from localhost to secondary1
+Write-Host "Copying AG certificates from localhost to pod $podags1"
+kubectl cp ag_certificate.cert ${NAMESPACE}/${podags1}:/var/opt/mssql
+kubectl cp ag_certificate.key  ${NAMESPACE}/${podags1}:/var/opt/mssql
+
+# Copy certificates from localhost to secondary2
+Write-Host "Copying AG certificates from localhost to pod $podags2"
+kubectl cp ag_certificate.cert ${NAMESPACE}/${podags2}:/var/opt/mssql
+kubectl cp ag_certificate.key  ${NAMESPACE}/${podags2}:/var/opt/mssql
